6b7bb2d470fc1d12db58e1d5db64062054c10f10
Jack Gardner <jack.gardner@fivebyfiveuk.com>
2016-05-04 16:25:34 +0100

    fix

--- react-mmmt-swiper.js
+++ react-mmmt-swiper.js
@@ -1,88 +1,97 @@
-"use strict";
+'use strict'
 
-(function (root, factory) {
-  if (typeof module !== "undefined" && module.exports) {
-	module.exports = factory(
-	  require("react"),
-	  require("react-dom"),
-	  require("swiper")
-	);
-  } else {
-	root.ReactSwiper = factory(
-	  root.React,
-	  root.Swiper
-	);
-  }
-})(this, function (React, ReactDOM, Swiper) {
-  var styles = {
-	container: {
-	},
+(function(root, factory) {
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = factory(
+            require('react'),
+            require('react-dom'),
+            require('swiper')
+        )
+    } else {
+        root.ReactSwiper = factory(
+            root.React,
+            root.Swiper
+        )
+    }
+})(this, function(React, ReactDOM, Swiper) {
+    var styles = {
+        container: {},
 
-	wrapper: {
-	  alignItems: "center"
-	},
+        wrapper: {
+            alignItems: 'center'
+        },
 
-	child: {
-	}
-  };
+        child: {}
+    }
 
-  var ReactSwiper = React.createClass({
-	// http://www.idangero.us/swiper/api
-	propTypes: {
-	  simulateTouch: React.PropTypes.bool,
-	  initialSlide: React.PropTypes.number,
-	  preloadImages: React.PropTypes.bool,
-	  lazyLoading: React.PropTypes.bool,
-	  lazyLoadingInPrevNext: React.PropTypes.bool,
-	  lazyLoadingOnTransitionStart: React.PropTypes.bool,
-	  pagination: React.PropTypes.node
-	},
+    var ReactSwiper = React.createClass({
+        // http://www.idangero.us/swiper/api
+        propTypes: {
+            simulateTouch: React.PropTypes.bool,
+            initialSlide: React.PropTypes.number,
+            preloadImages: React.PropTypes.bool,
+            lazyLoading: React.PropTypes.bool,
+            lazyLoadingInPrevNext: React.PropTypes.bool,
+            lazyLoadingOnTransitionStart: React.PropTypes.bool,
+            pagination: React.PropTypes.node
+        },
 
-	componentDidMount: function () {
-	  if (this.isMounted()) {
-		var props = Object.assign({}, this.props)
-		delete props.children
+        componentDidMount: function() {
+            if (this.isMounted()) {
+                var props = Object.assign({}, this.props)
+                delete props.children
 
-		if (this.props.pagination) {
+                if (this.props.pagination) {
 
-			props.pagination = ReactDOM.findDOMNode(this.refs.pagination)
-		}
+                    props.pagination = ReactDOM.findDOMNode(this.refs.pagination)
+                }
 
-		this.swiper = Swiper(ReactDOM.findDOMNode(this), props);
-		// NOTE: Does not seem that initialSlide works with images
-		// NOTE: Lazy loading is also not helped
-		var initialSlide = this.props.initialSlide;
-		if (initialSlide) {
-		  this.swiper.on("imagesReady", function() {
-			this.swiper.slideTo(initialSlide);
-		  }.bind(this));
-		}
-	  }
-	},
+                this.swiper = Swiper(ReactDOM.findDOMNode(this), props)
+                // NOTE: Does not seem that initialSlide works with images
+                // NOTE: Lazy loading is also not helped
+                var initialSlide = this.props.initialSlide
+                if (initialSlide) {
+                    this.swiper.on('imagesReady', function() {
+                        this.swiper.slideTo(initialSlide)
+                    }.bind(this))
+                }
+            }
+        },
 
-	componentDidUpdate: function () {
-	},
+        componentDidUpdate: function() {},
 
-	componentWillUnmount: function () {
-	  this.swiper.destroy();
-	  delete this.swiper;
-	},
+        componentWillUnmount: function() {
+            this.swiper.destroy()
+            delete this.swiper
+        },
 
-	shouldComponentUpdate: function (nextProps) {
-	  return (typeof this.props.shouldUpdate !== "undefined") && this.props.shouldUpdate(nextProps);
-	},
+        shouldComponentUpdate: function(nextProps) {
+            return (typeof this.props.shouldUpdate !== 'undefined') && this.props.shouldUpdate(nextProps)
+        },
 
-	render: function() {
-	  return React.createElement("div", Object.assign({}, this.props, {style: styles.container, className: "swiper-container" + (this.props.className ? ' ' + this.props.className : '')}),
-	    [React.createElement("div", {style: styles.wrapper, className: "swiper-wrapper"},
-		  React.Children.map(this.props.children, function (child, index) {
-			return React.cloneElement(child, { style: Object.assign(child.props.style || {}, styles.child), className: "swiper-slide " + child.props.className, key: index });
-		  })
-	    ),
-	    this.props.pagination ? React.cloneElement(this.props.pagination, {className: "swiper-pagination" + (this.props.pagination.props.className ? ' ' + this.props.pagination.props.className : ''), ref: "pagination"}) : null]
-	  );
-	}
-  });
+        render: function() {
+            return React.createElement('div', Object.assign({}, this.props, {
+                style: styles.container,
+                className: 'swiper-container' + (this.props.className ? ' ' + this.props.className : '')
+            }), [React.createElement('div', {
+                        style: styles.wrapper,
+                        className: 'swiper-wrapper'
+                    },
+                    React.Children.map(this.props.children, function(child, index) {
+                        return React.cloneElement(child, {
+                            style: Object.assign(child.props.style || {}, styles.child),
+                            className: 'swiper-slide ' + child.props.className
+                        })
+                    })
+                ),
+                this.props.pagination ? React.cloneElement(this.props.pagination, {
+                    className: 'swiper-pagination' + (this.props.pagination.props.className ? ' ' + this.props.pagination.props.className : ''),
+                    ref: 'pagination',
+                    key: 'pagination'
+                }) : null
+            ])
+        }
+    })
 
-  return ReactSwiper;
-});
+    return ReactSwiper
+})
